f89c5b840567d568dd359ab746e67ed3dd58f42a
ASUS <ASUS@LAPTOP-2JE99TTR>
2020-09-18 15:50:55 +0700

    creat

---
+++ Project0118092020/Source.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main() {
+	int i, j, n;
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++) {
+		for (j = 1; j <= n; j++) {
+			if (i == 1 || i == n || j == 1 || j == n)
+				printf("*");
+			else printf("#");
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
